f8baa508257bbcb81cb5f55dc7f4533165a4031f
ZuevaAlina <alina9111347@gmail.com>
2021-06-10 08:15:06 +0300

    Done 4-2

--- task_4.2/Main.java
+++ task_4.2/Main.java
@@ -14,11 +14,34 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        Dog dog = new Dog("bim",13);
+        System.out.println(dog.getName()+","+dog.getAge());
     }
 }
 
-public class Dog {
-    //напишите тут ваш код
+class Dog {
+    String name;
+    int age;
+
+    public Dog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
 
