3841290e64734d07d148cef44cd83e91318f3451
ZuevaAlina <alina9111347@gmail.com>
2021-06-10 11:30:48 +0300

    Done 1-3

--- task_1.3/Main.java
+++ task_1.3/Main.java
@@ -1,6 +1,8 @@
 /* Объяви переменные a, b типа int и переменную s типа String.
 Сразу же в строке объявления присвой им любые разные значения. */
 
+//проверка связи
+
 public class Main {
     public static void main(String[] args) {
         int a = 1;
