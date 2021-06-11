aae6b17092783df1220f82fe88520bbab5500c08
ZuevaAlina <alina9111347@gmail.com>
2021-06-11 13:09:14 +0300

    Done 4-8

--- task_4.8/Hotel.java
+++ task_4.8/Hotel.java
@@ -1,2 +1,72 @@
-package PACKAGE_NAME;public class Hotel {
+public class Hotel {
+    Room[] rooms;
+
+    public Hotel(Room[] rooms) {
+        this.rooms = rooms;
+    }
+
+    //вывод на экран списка свободных комнат
+    public void getFreeRooms(){
+        String freeRoomsList = "";
+        for (int i = 0; i < rooms.length; i++) {
+            if (rooms[i].isFree()) freeRoomsList += rooms[i].getRoomNumber()+", ";
+        }
+        System.out.println("Номера свободных комнат "+freeRoomsList);
+    }
+    //-------начало моего кода
+    //освобождение комнаты
+    public void vacateRoom(byte roomNumber){
+        String info = "Эта комната свободна";
+        for (int i = 0; i < rooms.length; i++) {
+            Room room = rooms[i];
+            if(room.getRoomNumber() == roomNumber && !room.isFree()){
+                room.setFree(true);
+                info = ("Комната номер "+roomNumber+" теперь свободна");
+                break;
+            }else if(room.getRoomNumber() == roomNumber && room.isFree()){
+                info = "Комната "+roomNumber+" и так свободна";
+                break;
+            }
+        }
+        System.out.println(info);
+    }
+    //вывести свойства комнаты
+    public void propertiesRoom(byte roomNumber){
+        String propertiesList = "";
+        for (int i = 0; i < rooms.length; i++) {
+            Room room = rooms[i];
+            if (room.getRoomNumber() == roomNumber) {
+                propertiesList += "\n Номер комнаты: \t"+rooms[i].getRoomNumber()+" ; \n "+
+                        "Количество мест: \t"+rooms[i].getQuantity()+" ; \n "+
+                        "Санузел: \t"+rooms[i].isWc()+" ; \n "+
+                        "Питание: \t"+rooms[i].isEat()+" ; \n "+
+                        "WiFi: \t"+rooms[i].isWifi()+" ; \n "+
+                        "Свободен: \t"+rooms[i].isFree()+" ;";
+            }else{
+                System.out.println("Такой комнаты нет");
+                break;
+            }
+
+        }
+        System.out.println(propertiesList);
+    }
+
+    //-------окончание моего кода
+
+        //бронирование комнаты
+    public void reserveRoom(byte roomNumber){
+        String info = "Такой комнаты не существует";
+        for (int i = 0; i < rooms.length; i++) {
+            Room room = rooms[i];
+            if(room.getRoomNumber() == roomNumber && room.isFree()){
+                room.setFree(false);
+                info = ("Комната номер "+roomNumber+" успешно забронированна");
+                break;
+            }else if(room.getRoomNumber() == roomNumber && !room.isFree()){
+                info = "Комната "+roomNumber+" занята";
+                break;
+            }
+        }
+        System.out.println(info);
+    }
 }
--- task_4.8/Main.java
+++ task_4.8/Main.java
@@ -1,25 +1,58 @@
+import java.util.Scanner;
+
 /*
-Реализуй в классе Fox интерфейс Animal.
-Поменяй код так, чтобы в классе Fox был только один метод - getName.
-Учти, что создавать дополнительные классы и удалять методы нельзя!
-
-Требования:
-•	Интерфейс Animal должен быть реализован в классе Fox.
-•	В интерфейсе Animal нужно объявить метод getColor.
-•	Дополнительные классы или интерфейсы создавать нельзя.
-*/
-
+ *  ***Гостиница***
+ *  1) Гостиница
+ *  2) Номера (кол-во мест(1-3), сан.узел(есть/нет), питаение(есть/нет), wifi(есть/нет), свободен/занят)
+ *
+ *   *Задание для лабораторной работы №4*
+ *   Освободить комнату
+ *   Вывести свойства комнаты
+ *   Показать комнаты с WiFi
+ *   Показать комнаты с WC
+ *   Показать комнаты с Eat
+ *   Показать комнаты по кол-ву спальных мест
+ *   Отобразить список команд
+ * */
 public class Main {
-    public static void main(String[] args){
-    }
-}
+    public static void main(String[] args) {
+        Room[] rooms = {
+                new Room((byte) 1,false,false,true,(byte) 11),
+                new Room((byte) 2,true,true,false,(byte) 12),
+                new Room((byte) 1,false,true,true,(byte) 13),
+                new Room((byte) 3,true,false,false,(byte) 21),
+                new Room((byte) 2,false,false,false,(byte) 22),
+                new Room((byte) 1,true,true,true,(byte) 23),
+                new Room((byte) 3,false,true,false,(byte) 31),
+                new Room((byte) 3,true,true,false,(byte) 32),
+                new Room((byte) 1,false,false,true,(byte) 33),
+        };
+        Hotel hotel = new Hotel(rooms);
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        while (true){
+            System.out.println("Введите команду");
+            command = scanner.nextLine();
+            if(command.equals("getFreeRooms")){
+                hotel.getFreeRooms();
+            }else if(command.equals("reserveRoom")){
+                System.out.print("Введите номер комнаты для бронирования: ");
+                byte roomNumber = (byte) scanner.nextInt();
+                hotel.reserveRoom(roomNumber);
+//начало моего кода
+            }else if(command.equals("vacateRoom")){
+                System.out.print("Введите номер комнаты, которая освободится: ");
+                byte roomNumber = (byte) scanner.nextInt();
+                hotel.vacateRoom(roomNumber);
+            }else if(command.equals("propertiesRoom")){
+                System.out.print("Введите номер комнаты, для вывода свойств: ");
+                byte roomNumber = (byte) scanner.nextInt();
+                hotel.propertiesRoom(roomNumber);
+//окончание моего кода
+            }else if (command.equals("exit")){
+                break;
+            }
+        }
 
-interface Animal {
-    Color getColor();
-}
-    
-class Fox {
-    public String getName() {
-        return "Fox";
     }
 }
