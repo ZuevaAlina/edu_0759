f7559428c350e018700f4f4daaffeb8d3daaa34b
ZuevaAlina <alina9111347@gmail.com>
2021-06-11 08:17:50 +0300

    Done 2-7

--- task_2.7/Main.java
+++ task_2.7/Main.java
@@ -4,6 +4,9 @@
  числа от 2 до 30. Если число не делится - выведите 'false', а если делится - выведите 'true'.
 */
 
+//проверка связи
+
+
 public class Main {
     public static void main(String[] args) {
         int a = 31;
