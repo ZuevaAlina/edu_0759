6fd9808e7c5ef238493a20da91a50d8d1349f2f3
ZuevaAlina <alina9111347@gmail.com>
2021-06-12 11:00:05 +0300

    Done 5-3

--- task_5.3/Main.java
+++ task_5.3/Main.java
@@ -1,11 +1,23 @@
 /*
-* Задание: Дана коллекция с числами. Запишите в новую коллекцию только те числа, которые больше нуля и меньше 10-ти.
+* Задание: Дана коллекция с числами. Запишите в новую коллекцию только те числа,
+* которые больше нуля и меньше 10-ти.
 * Коллекции вы создаёте сами
 */
 
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-    
+    ArrayList<Integer> nums = new ArrayList<>();
+    ArrayList<Integer> nums1 = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            nums.add(i);
+            }
+        for (int i = 0; i < nums.size(); i++) {
+            if ((nums.get(i)>0)&&(nums.get(i)<10)){
+                nums1.add(nums.get(i));
+            }
+        }
+        System.out.println(nums);
+        System.out.println(nums1);
     }
 }
