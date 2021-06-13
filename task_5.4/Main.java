a6410a61b64f6f7af62d09e1098e8151d59665b5
ZuevaAlina <alina9111347@gmail.com>
2021-06-13 21:10:28 +0300

    Done 5-4

--- task_5.4/Main.java
+++ task_5.4/Main.java
@@ -18,9 +18,19 @@
 8
 10
 */
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        // напишите тут ваш код
+        try(FileInputStream fin=new FileInputStream("/Users/alinocka/Desktop/JAVA/test.txt")) {
+            System.out.printf("File size: %d bytes \n", fin.available());
+            int i=-1;
+            while((i=fin.read())!=-1){
+                System.out.print((char)i);
+            }
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
